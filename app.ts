2bff22cbedbfac523551020f79cae5f7ad78c5ee
pavantechally <pavankumar.v@techallylabs.com>
2023-10-29 22:47:21 +0530

    added

---
+++ app.ts
@@ -0,0 +1,104 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors'
+import path from 'path'
+import http from 'http'
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'Acl1LA-uavQg-Yk9m7tW6Z4a-PQemT4_BvxUUTvfX-1cb2gkxK2Q_NyHX7NEwVvJOnMGVX-oFEDkJ7Fe',
+    'client_secret': 'EPtlBcMfzWzuXHPClSdCNETQ6oLYETh1gqM_aBa9U9KxL-daH1e1W47T0KdGmWLwf3Sf5aRB6UPPU1Oc'
+  });
+
+  
+app.get('/', (req:Request, res:Response) => res.sendFile(__dirname + "/index.html"));
+
+
+app.post('/pay', async (req:Request, res:Response) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Redhock Bar Soap",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "Washing Bar soap"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error:any, payment:any) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  
+  });
+
+
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+  
+  // Obtains the transaction details from paypal
+    paypal.payment.execute(paymentId, execute_payment_json, function (error:any, payment:any) {
+        //When error occurs when due to non-existent transaction, throw an error else log the transaction details in the console then send a Success string reposponse to the user.
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+
+
+// app.get('/', (req:Request,res:Response) => {
+//     try {
+
+
+//         res.send('hello world')
+//     }
+//     catch (err:any) {
+//         res.send({message:err?.message})
+//     }
+// })
+
+
+app.listen(9001,() => {console.log('listening on the localhost 9001')})
